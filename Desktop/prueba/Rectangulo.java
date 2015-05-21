e37f35c938b15acfc27f83aac93fd4cf7c4fc3ff
SolangeGalarza <kareng_5@hotmail.com>
2015-05-21 17:07:22 -0500

    Update Rectangulo.java

--- Desktop/prueba/Rectangulo.java
+++ Desktop/prueba/Rectangulo.java
@@ -1,45 +1,50 @@
 package figuras;
 
+
 public class Rectangulo {
     
-    private double base;
-    private double altura;
-    private double areaRectangulo;
-
-    public Rectangulo(double base, double altura) 
-    {
-        this.base = base;
-        this.altura = altura;
-    }
-
-    public double getBase() 
-    {
+    public  static double base;
+    public static double altura;
+    public static double area;
+    public static double perimetro;
+    
+   //Mètodos get & set
+    public double getBase() {
         return base;
     }
-
-    public void setBase(double base)
-    {
+    public void setBase(double base) {
         this.base = base;
     }
-
-    public double getAltura() 
-    {
+    public double getAltura() {
         return altura;
     }
-
-    public void setAltura(double altura) 
-    {
+    public void setAltura(double altura) {
         this.altura = altura;
     }
-
-    public double getAreaRectangulo() 
-    {
-        return areaRectangulo;
+    public double getArea() {
+        return area;
+    }
+    public void setÁrea(double area) {
+        this.area = area;
+    }
+    public double getPerimetro() {
+        return perimetro;
+    }
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+    // CALCULO DE ÀREAS Y PERÌMETROS
+    
+    public static double CalcularPerimetro(){
+     perimetro=base+base+altura+altura;
+     return perimetro;
+    }
+    public static double CalcularArea() {
+     area=(base*altura);
+     return area;
     }
     
-    public void area() 
-    {
-        areaRectangulo = base * altura;
+    public  static void Imprimir(){  
+    System.out.println("EL ÁREA ES: "+area+"\n EL PERÍMETRO ES: "+perimetro);
     }
 }
-
